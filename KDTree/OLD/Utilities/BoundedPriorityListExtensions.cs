19c6217ddffe52e810fda2f3c9e3fa263fe2a12e
Matt Campbell <matt.campbell@oregonstate.edu>
2025-08-19 21:01:36 -0700

    it didn't seem that adding Span in placed improved anything. sometimes better somtimes worse. that's the end of this branch

---
+++ KDTree/OLD/Utilities/BoundedPriorityListExtensions.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="BoundedPriorityListExtensions.cs" company="Eric Regina">
+// Copyright (c) Eric Regina. All rights reserved.
+// </copyright>
+
+namespace SuperClusterKDTree.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Formats.Asn1;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Contains extension methods for <see cref="BoundedPriorityList{TElement,TPriority}"/> class.
+    /// </summary>
+    internal static class BoundedPriorityListExtensions
+    {
+
+    }
+}
